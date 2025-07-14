e386d9d06bbbf167b7d436c5e83ebfb926afe368
Pequeno Maxence <maxence.pequeno@icloud.com>
2025-07-15 01:01:16 +0200

    Premier test des sessions, ajout d'un bouton et test d'affichage. Ajout du systeme de session et création du reloader

--- public/basepage.php
+++ public/basepage.php
@@ -1,1 +1,39 @@
 <?php
+declare(strict_types=1);
+
+/* Debut de la session */
+session_start();
+
+use Maxence\BakerySimulator\HTML\webpage;
+
+$webpage = new webpage();
+$webpage->setTitle("Ma Boulangerie");
+$webpage->appendCssUrl('http://localhost:8000/css/globalstyle.css');
+
+/* Initialisation des variables si première visite  */
+if (!isset($_SESSION['breadAmount'])) {
+    $_SESSION['breadAmount'] = 0;
+}
+if (!isset($_SESSION['clickMultiplication'])) {
+    $_SESSION['clickMultiplication'] = 1;
+}
+
+/*  Traitement de l'action  */
+if (isset($_POST['faire_pain'])) {
+    $_SESSION['breadAmount'] += $_SESSION['clickMultiplication'];
+}
+
+/* Rechargement automatique de la page toutes les 1 seconde */
+    /* Obligatoire pour les autoclickers sans javascript */
+$webpage->appendToHead('<meta http-equiv="refresh" content="1">');
+
+/* Affichage HTML  */
+$webpage->appendContent(<<<HTML
+<form method="post">
+    <button type="submit" name="faire_pain">Faire un pain 🥖</button>
+</form>
+
+<p>Pains en stock : {$_SESSION['breadAmount']}</p>
+HTML);
+
+echo $webpage->toHtml();
