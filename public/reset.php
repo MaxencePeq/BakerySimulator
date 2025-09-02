6f2a9aa1bbf53e626ee0e7c13ab887e5578a51ed
Pequeno Maxence <maxence.pequeno@icloud.com>
2025-09-02 15:04:28 +0200

    Simplification du systeme de reset

--- public/reset.php
+++ public/reset.php
@@ -23,44 +23,6 @@ $_SESSION['user_id'] = $userId;
 $_SESSION['username'] = $username;
 
 /* -----------------------------
-   Variables de jeu par défaut
-------------------------------*/
-
-// Ressources
-$_SESSION['bread'] = 0;
-$_SESSION['flour'] = 0;
-$_SESSION['money'] = 0;
-
-// Production de base
-$_SESSION['breadPerClick'] = 1;
-$_SESSION['breadPrice'] = 1.0;
-
-// Automatisation de base
-$_SESSION['autoClickers'] = 0;
-$_SESSION['autoSellers'] = 0;
-$_SESSION['autoFlourBuyers'] = 0;
-
-// Améliorations (aucune achetée au départ)
-$_SESSION['Bought_cost_addAmount1'] = false;
-$_SESSION['Bought_cost_addAmount2'] = false;
-$_SESSION['Bought_cost_addAmount3'] = false;
-
-$_SESSION['Bought_cost_multi1'] = false;
-$_SESSION['Bought_cost_multi2'] = false;
-$_SESSION['Bought_cost_multi3'] = false;
-
-$_SESSION['Bought_cost_AutoClick1'] = false;
-$_SESSION['Bought_cost_AutoClick2'] = false;
-
-$_SESSION['Bought_cost_UpPrice1'] = false;
-$_SESSION['Bought_cost_AutoSeller1'] = false;
-$_SESSION['Bought_cost_AutoFlourBuyer1'] = false;
-
-// Interface par défaut
-$_SESSION['showStats'] = true;
-$_SESSION['showAugment'] = true;
-
-/* -----------------------------
    Écrase la sauvegarde en BDD
 ------------------------------*/
 $pdo = MyPDO::getInstance();
@@ -71,5 +33,5 @@ $stmt->execute([
 ]);
 
 // Redirection vers la partie
-header('Location: basepage.php');
+header('Location: index.php');
 exit;
